f0812a2d7adf7cec041ded6ae548db3dd67f08c8
Ahzam Akhtar <ahzam8186@gmail.com>
2022-11-13 00:38:53 +0530

    UPdateed ui for payment page

--- pages/Products/News/Payment.js
+++ pages/Products/News/Payment.js
@@ -80,208 +80,238 @@ const Payment = () => {
   
   return (
     <>
-      <div class="container px-5 py-5 mx-auto flex flex-wrap">
-        <div class="flex w-full  flex-wrap">
-          <h1 class="sm:text-2xl text-xl  title-font lg:w-1/3 lg:mb-0 mb-4">
-            PAYMENT PAGE
-          </h1>
-        </div>
-      </div>
+      <section>
+        <div class="container py-5">
+          <div class="card">
+            <div class="card-body">
+              <div class="row d-flex justify-content-center pb-5">
+                <div class="col-md-7 col-xl-5 mb-4 mb-md-0">
+                  <div class="py-4 d-flex flex-row">
+                    <h5>
+                      <span class="far fa-check-square pe-2"></span>
+                      <b>ELIGIBLE</b> |
+                    </h5>
+                    <span class="ps-2">Pay</span>
+                  </div>
+                  <h4 class="text-success">{"$"+amount}</h4>
+                  <h4>Diabetes Pump & Supplies</h4>
 
-      <section class="text-gray-600 body-font mx-2">
-        <div class="container px-2 py-5 mx-auto flex flex-wrap">
-          <div class="flex flex-wrap -m-4">
-            <div class="p-2  lg:w-1/2 md:w-full">
-              <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
-                <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-8 h-8"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                  </svg>
-                </div>
-                <div class="flex-grow">
-                  <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
-                    Shooting Stars
-                  </h2>
-                  <p class="leading-relaxed text-base">
-                    Blue bottle crucifix vinyl post-ironic four dollar toast
-                    vegan taxidermy. Gastropub indxgo juice poutine.
+                  <p>
+                    Insurance claims and all necessary dependencies will be
+                    submitted to your insurer for the coverred portion of this
+                    order
                   </p>
-                  <button
-                    type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                  >
-                    Change Plan
-                  </button>
-                  <button
-                    type="button"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div class="p-2 lg:w-1/2 md:w-full">
-              <div class="flex border-2 rounded-lg border-gray-200 border-opacity-50 p-8 sm:flex-row flex-col">
-                <div class="w-16 h-16 sm:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-10 h-10"
-                    viewBox="0 0 24 24"
-                  >
-                    <HiDocumentText className="text-2xl" />
-                  </svg>
-                </div>
-                <div class="flex-grow">
-                  <h2 class="text-gray-900 text-lg title-font font-medium mb-3 mt-3">
-                    YOUR ORDER
-                  </h2>
-                  <div class="container text-center">
-                    <div class="row align-items-start">
+
+                  <hr />
+                  <section class="gradient-custom">
+                    <div class="container ">
+                      <div class="row d-flex justify-content-center ">
+                        <div class="col-md-10 col-lg-10 col-xl-12">
+                          <div class="card">
+                            <div class="card-body p-4">
+                              <form>
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                  <div class="form-outline">
+                                    <input
+                                      type="text"
+                                      id="typeText"
+                                      class="form-control form-control-lg"
+                                      siez="17"
+                                      placeholder="1234 5678 9012 3457"
+                                      minlength="19"
+                                      maxlength="19"
+                                    />
+                                    <label class="form-label" for="typeText">
+                                      Card Number
+                                    </label>
+                                  </div>
+                                  <img
+                                    src="https://img.icons8.com/color/48/000000/visa.png"
+                                    alt="visa"
+                                    width="64px"
+                                  />
+                                </div>
+
+                                <div class="d-flex justify-content-between align-items-center mb-4">
+                                  <div class="form-outline">
+                                    <input
+                                      type="text"
+                                      id="typeName"
+                                      class="form-control form-control-lg"
+                                      siez="17"
+                                      placeholder="Cardholder's Name"
+                                    />
+                                    <label class="form-label" for="typeName">
+                                      Cardholder's Name
+                                    </label>
+                                  </div>
+                                </div>
+
+                                <div class="d-flex justify-content-between align-items-center pb-2">
+                                  <div class="form-outline">
+                                    <input
+                                      type="text"
+                                      id="typeExp"
+                                      class="form-control form-control-lg"
+                                      placeholder="MM/YYYY"
+                                      size="7"
+                                      minlength="7"
+                                      maxlength="7"
+                                    />
+                                    <label class="form-label" for="typeExp">
+                                      Expiration
+                                    </label>
+                                  </div>
+                                  <div class="form-outline">
+                                    <input
+                                      type="password"
+                                      id="typeText2"
+                                      class="form-control form-control-lg"
+                                      placeholder="&#9679;&#9679;&#9679;"
+                                      size="1"
+                                      minlength="3"
+                                      maxlength="3"
+                                    />
+                                    <label class="form-label" for="typeText2">
+                                      Cvv
+                                    </label>
+                                  </div>
+                                  <button
+                                    type="button"
+                                    class="btn btn-info btn-lg btn-rounded"
+                                  >
+                                    <i class="fas fa-arrow-right"></i>
+                                  </button>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row p-2 mt-5">
                       <div class="col">
-                        Product
-                        <p className="mt-4">News</p>
+                        <div class="form-outline">
+                          <input
+                            type="text"
+                            id="form8Example1"
+                            class="form-control"
+                          />
+                          <label class="form-label" for="form8Example1">
+                            Name
+                          </label>
+                        </div>
                       </div>
                       <div class="col">
-                        Type
-                        <p className="mt-4">{type.toUpperCase()}</p>
+                        <div class="form-outline">
+                          <input
+                            type="email"
+                            id="form8Example2"
+                            class="form-control"
+                          />
+                          <label class="form-label" for="form8Example2">
+                            Email address
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+
+                    <hr />
+
+                    <div class="row p-2 mt-5">
+                      <div class="col">
+                        <div class="form-outline">
+                          <input
+                            type="text"
+                            id="form8Example3"
+                            class="form-control"
+                          />
+                          <label class="form-label" for="form8Example3">
+                            First name
+                          </label>
+                        </div>
                       </div>
                       <div class="col">
-                        AMOUNT
-                        <p className="mt-4">{"$" + amount}</p>
+                        <div class="form-outline">
+                          <input
+                            type="text"
+                            id="form8Example4"
+                            class="form-control"
+                          />
+                          <label class="form-label" for="form8Example4">
+                            Last name
+                          </label>
+                        </div>
+                      </div>
+                      <div class="col">
+                        <div class="form-outline">
+                          <input
+                            type="email"
+                            id="form8Example5"
+                            class="form-control"
+                          />
+                          <label class="form-label" for="form8Example5">
+                            Email address
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </section>
+                </div>
+
+                <div class="col-md-5 col-xl-4 offset-xl-1">
+                  <div class="py-4 d-flex justify-content-end">
+                    <button type="button" class="btn btn-success mx-3" onClick={()=>{
+                      pay()
+                      updateSubscriptionStatus()
+                    }}>
+                      Proceed
+                    </button>
+                    <button type="button" class="btn btn-danger">
+                      Cancel
+                    </button>
+                  </div>
+                  <div class="rounded d-flex flex-column p-2">
+                    <div class="p-2 me-3">
+                      <h4>Order Recap</h4>
+                    </div>
+                    <div class="p-2 d-flex">
+                      <div class="col-8">Contracted Price</div>
+                      <div class="ms-auto">{"$" + amount}</div>
+                    </div>
+                    <div class="p-2 d-flex">
+                      <div class="col-8">TAX</div>
+                      <div class="ms-auto">$0.00</div>
+                    </div>
+                    <div class="p-2 d-flex">
+                      <div class="col-8">Product Type</div>
+                      <div class="ms-auto">{type.toUpperCase()}</div>
+                    </div>
+                    
+                    <div class="border-top px-2 mx-2"></div>
+
+                    <div class="p-2 d-flex">
+                      <div class="col-8">
+                        Payable Amount{" "}
+                        <span class="fa fa-question-circle text-dark"></span>
+                      </div>
+                      <div class="ms-auto">
+                        <b>{"$"+amount}</b>
+                      </div>
+                    </div>
+                    <div class="border-top px-2 mx-2"></div>
+                    <div class="p-2 d-flex pt-3">
+                      <div class="col-8">
+                        <b>Total</b>
+                      </div>
+                      <div class="ms-auto">
+                        <b class="text-success">{"$"+amount}</b>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-
-      <section class="text-gray-600 body-font ">
-        <div class="container px-5 mx-auto">
-          <div class="flex flex-wrap w-full flex-col items-center text-center">
-            <div class="w-full h-full  sm:mr-8 sm:mb-0  items-center justify-center rounded-full bg-white text-indigo-500 flex-shrink-0">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                class="w-8 h-8"
-                viewBox="0 0 24 24"
-              ></svg>
-              <h1 class="sm:text-3xl  text-2xl font-medium title-font  text-gray-900">
-                <MdPayment className="mx-auto text-5xl" />{" "}
-                {"SubTotal : " + "$" + amount}
-              </h1>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section class="text-gray-600 body-font relative">
-        <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-          <div class="lg:w-1/2 px-4 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
-              General Details
-            </h2>
-
-            <div class="relative mb-4">
-              <label for="name" class="leading-7 text-sm text-gray-600">
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div class="relative mb-4">
-              <label for="email" class="leading-7 text-sm text-gray-600">
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div class="relative mb-4">
-              <label for="message" class="leading-7 text-sm text-gray-600">
-                Billing Address
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              ></textarea>
-            </div>
-          </div>
-
-          <div class="lg:w-1/2 px-4 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-            <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
-              Card Details
-            </h2>
-
-            <div class="relative mb-4">
-              <label for="name" class="leading-7 text-sm text-gray-600">
-                Card Number
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div class="relative mb-4">
-              <label for="email" class="leading-7 text-sm text-gray-600">
-                CVV
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div class="relative mb-4">
-              <label for="message" class="leading-7 text-sm text-gray-600">
-                EX
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <button
-              onClick={()=>{
-                pay()
-                updateSubscriptionStatus()
-                
-              }}
-              class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-            >
-              Pay {"$" + amount}
-            </button>
           </div>
         </div>
       </section>
