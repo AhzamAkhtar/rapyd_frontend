cb6cc92d3a49d41d013bc2bf4ae45730863ab0ca
Ahzam Akhtar <ahzam8186@gmail.com>
2022-11-17 01:15:16 +0530

    update complete front part

--- pages/Products/News/Invoice.js
+++ pages/Products/News/Invoice.js
@@ -1,33 +1,7 @@
-import React,{useState}from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 const jwt = require("jsonwebtoken");
 const Invoice = () => {
-
-  //const [name, setname] = useState()
-
-  //const [phone, setphone] = useState()
-
-  ///const [email, setemail] = useState()
-
-  const changeHandler = (e) =>{
-    if(e.target.name=="name"){
-      setname(e.target.value)
-    }
-    if(e.target.name=="phone"){
-      setphone(e.target.value)
-    }
-    if(e.target.name=="email"){
-      setemail(e.target.value)
-    }
-  }
-
-  const router = useRouter();
-
-  
-  const { transactionId  , amount , type , name , receipt_email, phone_number , line_1  ,card} = router.query;  
-
- 
-  console.log(card)
   const secretKey = "secret123";
   const token = localStorage.getItem("token");
   const decode_JWT = jwt.decode(token);
@@ -35,10 +9,51 @@ const Invoice = () => {
   console.log(email);
   const data = { email };
 
+  const [updatedName, setupdatedName] = useState();
 
+  const [updatedPhone, setupdatedPhone] = useState();
+
+  const [updatedEmail, setupdatedEmail] = useState();
+
+  const [updatedAddress, setUpdatedAddress] = useState();
+
+  const [updateData, setUpdateData] = useState(false);
+
+  const changeHandler = (e) => {
+    if (e.target.name == "updatedName") {
+      setupdatedName(e.target.value);
+    }
+    if (e.target.name == "updatedPhone") {
+      setupdatedPhone(e.target.value);
+    }
+    if (e.target.name == "updatedEmail") {
+      setupdatedEmail(e.target.value);
+    }
+    if (e.target.name == "updatedAddress") {
+      setUpdatedAddress(e.target.value);
+    }
+  };
+
+  const router = useRouter();
+
+  const {
+    transactionId,
+    amount,
+    type,
+    name,
+    receipt_email,
+    phone_number,
+    line_1,
+    card,
+  } = router.query;
+
+  const updateUserData = async () => {
+    const responce = await fetch(`http://localhost:3005/update?receipt_email=${updatedEmail}&name=${updatedName}&line_1=${updatedAddress}&payment_id=${transactionId}`)
+    const res = await responce.json()
+    console.log(res)
+  }
 
   const updateSubscriptionStatus = async () => {
-
     let res = await fetch("http://localhost:3000/api/updatenewssub", {
       method: "POST",
       headers: {
@@ -48,12 +63,11 @@ const Invoice = () => {
     });
     let responce = await res.json();
     console.log(responce);
-
   };
 
-  const updateTransactionId = async (transactionId) =>{
-    const dataForTransactionUpdation = {email ,transactionId}
-    console.log(transactionId)
+  const updateTransactionId = async (transactionId) => {
+    const dataForTransactionUpdation = { email, transactionId };
+    console.log(transactionId);
     let res = await fetch("http://localhost:3000/api/updatetransactionid", {
       method: "POST",
       headers: {
@@ -63,27 +77,28 @@ const Invoice = () => {
     });
     let responce = await res.json();
     console.log(responce);
-    
-  }
+  };
 
-  const addNewsTransactionId = async (transactionIdnews) =>{
-    const dataForNewsTransactionId = {email , transactionIdnews}
-    let res = await fetch("http://localhost:3000/api/addnewspayid",{
-      method : "POST",
+  const addNewsTransactionId = async (transactionIdnews) => {
+    const dataForNewsTransactionId = { email, transactionIdnews };
+    let res = await fetch("http://localhost:3000/api/addnewspayid", {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body : JSON.stringify(dataForNewsTransactionId)
+      body: JSON.stringify(dataForNewsTransactionId),
     });
-    let responce = await res.json()
-    console.log(responce) 
-  }
+    let responce = await res.json();
+    console.log(responce);
+  };
 
-  const completePayment = async () =>{
-    const responce = await fetch(`http://localhost:3005/complete?token=${transactionId}`)
-    const res = await responce.json()
-    console.log(res)
-  }
+  const completePayment = async () => {
+    const responce = await fetch(
+      `http://localhost:3005/complete?token=${transactionId}`
+    );
+    const res = await responce.json();
+    console.log(res);
+  };
 
   return (
     <>
@@ -108,22 +123,17 @@ const Invoice = () => {
                         <div class="p-4 ">
                           <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-green-300">
                             <div class="p-6">
-                              
-                             {card.boolean ? (
-                              
+                              {card.boolean ? (
                                 <h6>PAID AMOUNT -</h6>
-                            
-                             ):(
-                            
+                              ) : (
                                 <h6>PAYABLE AMOUNT</h6>
-                           
-                             )}
-                              
+                              )}
+
                               <h1 class="title-font text-3xl font-medium text-gray-900 mb-3">
                                 ${amount}
                               </h1>
                               <p class="leading-relaxed mb-3 text-lg">
-                                {"Transaction Id  - "+transactionId}
+                                {"Transaction Id  - " + transactionId}
                               </p>
                             </div>
                           </div>
@@ -133,76 +143,103 @@ const Invoice = () => {
                   </section>
 
                   <hr />
-                  <section class="gradient-custom">
-                    <div class="container "></div>
-                    <div class="row p-2 mt-5">
-                      <div class="col">
-                        <div class="form-outline">
-                          <input
-                            type="text"
-                            id="phone"
-                            name="phone"
-                            value={phone_number}
-                            onChange= {changeHandler}
-                            class="form-control"
-                          />
-                          <label class="form-label" for="form8Example1">
-                            PhoneNumber
-                          </label>
-                        </div>
-                      </div>
-                      <div class="col">
-                        <div class="form-outline">
-                          <input
-                            type="email"
-                            id="email"
-                            value={receipt_email}
-                            name = "email"
-                            onChange= {changeHandler}
-                            class="form-control"
-                          />
-                          <label class="form-label" for="form8Example2">
-                            Email address
-                          </label>
-                        </div>
-                      </div>
-                    </div>
 
-                    <hr />
+                  {!updateData && (
+                    <button
+                      className="text-white bg-green-400 w-full border-0 py-2 px-6 mt-10 focus:outline-none hover:bg-green-600 rounded-3xl text-lg"
+                      onClick={() => {
+                        setUpdateData(true);
+                      }}
+                    >
+                      Update Data
+                    </button>
+                  )}
+                  {updateData ? (
+                    <>
+                      <section class="gradient-custom">
+                        <div class="container "></div>
+                        <div class="row p-2 mt-5">
+                          <div class="col">
+                            <div class="form-outline">
+                              <input
+                                type="number"
+                                id="updatedPhone"
+                                name="updatedPhone"
+                                value={updatedPhone}
+                                onChange={changeHandler}
+                                class="form-control"
+                              />
+                              <label class="form-label" for="form8Example1">
+                                PhoneNumber
+                              </label>
+                            </div>
+                          </div>
+                          <div class="col">
+                            <div class="form-outline">
+                              <input
+                                type="updatedEmail"
+                                id="updatedEmail"
+                                value={updatedEmail}
+                                name="updatedEmail"
+                                onChange={changeHandler}
+                                class="form-control"
+                              />
+                              <label class="form-label" for="form8Example2">
+                                Email address
+                              </label>
+                            </div>
+                          </div>
+                        </div>
 
-                    <div class="row p-2 mt-5">
-                      <div class="col">
-                        <div class="form-outline">
-                          <input
-                            type="text"
-                            id="name"
-                            onChange= {changeHandler}
-                            value={name}
-                            name = "name"
-                            class="form-control"
-                          />
-                          <label class="form-label" for="form8Example3">
-                            Address Name
-                          </label>
+                        <hr />
+
+                        <div class="row p-2 mt-5">
+                          <div class="col">
+                            <div class="form-outline">
+                              <input
+                                type="text"
+                                id="updatedName"
+                                onChange={changeHandler}
+                                value={updatedName}
+                                name="updatedName"
+                                class="form-control"
+                              />
+                              <label class="form-label" for="form8Example3">
+                                Address Name
+                              </label>
+                            </div>
+                          </div>
+                          <div class="col">
+                            <div class="form-outline">
+                              <input
+                                type="text"
+                                id="updatedAddress"
+                                value={updatedAddress}
+                                onChange={changeHandler}
+                                name="updatedAddress"
+                                class="form-control"
+                              />
+                              <label class="form-label" for="form8Example4">
+                                Address
+                              </label>
+                            </div>
+                          </div>
                         </div>
-                      </div>
-                      <div class="col">
-                        <div class="form-outline">
-                          <input
-                            type="text"
-                            id="address"
-                            value={line_1}
-                            name = "address"
-                            class="form-control"
-                          />
-                          <label class="form-label" for="form8Example4">
-                            Address
-                          </label>
-                        </div>
-                      </div>
-                      
-                    </div>
-                  </section>
+                      </section>
+
+                      <button
+                        className="text-white bg-green-400 w-full border-0 py-2 px-6 mt-10 focus:outline-none hover:bg-green-600 rounded-3xl text-lg"
+                        onClick={() => {
+                          setUpdateData(false);
+                          updateUserData()
+                        }}
+                      >
+                        Update
+                      </button>
+                    </>
+                  ) : (
+                    <></>
+                  )}
                 </div>
 
                 <div class="col-md-5 col-xl-4 offset-xl-1">
@@ -222,7 +259,6 @@ const Invoice = () => {
                       <div class="col-8">Product Type</div>
                       <div class="ms-auto">{type}</div>
                     </div>
-                   
 
                     <div class="border-top px-2 mx-2"></div>
 
@@ -231,7 +267,9 @@ const Invoice = () => {
                         Payable Amount{" "}
                         <span class="fa fa-question-circle text-dark"></span>
                       </div>
-                      <div class="ms-auto"><b>{"$"+amount}</b></div>
+                      <div class="ms-auto">
+                        <b>{"$" + amount}</b>
+                      </div>
                     </div>
                     <div class="border-top px-2 mx-2"></div>
                     <div class="p-2 d-flex pt-3">
@@ -239,27 +277,24 @@ const Invoice = () => {
                         <b>Total</b>
                       </div>
                       <div class="ms-auto">
-                        <b class="text-success">{"$"+amount}</b>
+                        <b class="text-success">{"$" + amount}</b>
                       </div>
                     </div>
                   </div>
-                  <button class="text-white bg-green-400 w-full border-0 py-2 px-6 mt-10 focus:outline-none hover:bg-green-600 rounded-3xl text-lg" onClick={() => {
+                  <button
+                    class="text-white bg-green-400 w-full border-0 py-2 px-6 mt-10 focus:outline-none hover:bg-green-600 rounded-3xl text-lg"
+                    onClick={() => {
                       //updateSubscriptionStatus()
                       //updateTransactionId(transactionId)
                       //addNewsTransactionId(transactionId)
                       //completePayment()
-                    }}> 
-                    {card.boolean ?(
-                      <p6>
-                    Paid ${amount}
-
-                      </p6>
-                    ):(
-                      <p6>
-                      Payable Amount ${amount}
-                      </p6>
+                    }}
+                  >
+                    {card.boolean ? (
+                      <p6>Paid ${amount}</p6>
+                    ) : (
+                      <p6>Payable Amount ${amount}</p6>
                     )}
-                    
                   </button>
                 </div>
               </div>
